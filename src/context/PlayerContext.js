94d5d1a6d70c69962575a4bc2b66f7b9aebd1bd9
karthikeyan G <karthikeyangnanasekar07@gmail.com>
2025-09-08 16:37:41 +0530

    Update PlayerContext.js

--- src/context/PlayerContext.js
+++ src/context/PlayerContext.js
@@ -7,7 +7,7 @@ const PlayerContextProvider = (props) => {
 	const audioRef = useRef();
 	const seekBg = useRef();
 	const seekBar = useRef();
-	const url = 'http://localhost:4000';
+	const url = 'https://backend-xyz.onrender.com';
 	const [songsData, setSongsData] = useState([]);
 	const [albumData, setAlbumData] = useState([]);
 	const [track, setTrack] = useState(null);
@@ -134,3 +134,4 @@ const PlayerContextProvider = (props) => {
 	);
 };
 export default PlayerContextProvider;
+
